e9f057217b57c051e5ba0cb4c43194bc745812cf
Roman <roman.blumovich@gmail.com>
2025-10-18 13:11:33 +0300

    blog page basic layout

---
+++ tc-marketplace/frontend/src/components/BlogCard.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+export default function BlogCard({ post }) {
+
+    return (
+        <div className="bg-white rounded-xl shadow hover:shadow-lg transition flex flex-col">
+            <img src={post.image} alt={post.title} className="h-48 w-full object-cover rounded-t-xl" />
+            <div className="p-4 flex flex-col flex-grow">
+                <h3 className="text-xl font-semibold text-green-900 mb-2">{post.title}</h3>
+                <p className="text-gray-700 text-sm flex-grow">{post.excerpt}</p>
+                <div className="mt-4 flex justify-between text-sm text-gray-500">
+                    <span>{post.author}</span>
+                    <span>{new Date(post.date).toLocaleDateString()}</span>
+                </div>
+                <Link
+                to={`/blog/${post.id}`}
+                className="mt-4 inline-block text-green-700 hover:text-green-900 font-medium"
+                >
+                Read More →
+                </Link>
+            </div>
+        </div>
+    );
+
+}
---
+++ tc-marketplace/frontend/src/pages/Blog.jsx
@@ -0,0 +1,22 @@
+import BlogCard from "../components/BlogCard";
+import Navbar from "../components/Navbar";
+import { blogPosts } from "../data/blogPosts";
+
+const Blog = () => {
+    return (
+    <section className="bg-slate-50 py-6 px-6 text-green-950">
+        <Navbar theme="light" />
+        <div className="container max-w-[1100px] mx-auto pt-8">
+            <h1 className="text-3xl font-bold text-center mb-2.5">Our Blog</h1>
+            <p className="text-center text-[20px]">Grow your knowledge — discover tips, techniques, and stories from experienced tissue culture enthusiasts.</p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
+            {blogPosts.map(post => (
+                <BlogCard key={post.id} post={post} />
+            ))}
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default Blog;
