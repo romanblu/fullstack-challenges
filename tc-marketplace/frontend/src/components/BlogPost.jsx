9bd3eefd1d09618217dc2c55c5a19c7c4998e729
Roman <roman.blumovich@gmail.com>
2025-10-19 13:35:30 +0300

    added custom theme for blog post

--- tc-marketplace/frontend/src/components/BlogPost.jsx
+++ tc-marketplace/frontend/src/components/BlogPost.jsx
@@ -19,14 +19,14 @@ export default function BlogPost({slug }) {
     console.log(post);
     return (
         <div>
-            <Navbar theme="light" />
-            <div className="max-w-3xl mx-auto py-12 px-6">
+            
+            <div className="max-w-4xl mx-auto py-12 px-6">
                 <h1 className="text-4xl font-bold mb-4 text-green-900">{post.attributes.title}</h1>
                 <p className="text-gray-600 mb-8">{post.attributes.excerpt}</p>
                 <p className="text-gray-600 mb-8">{post.attributes.date}</p>
                 <p className="text-gray-600 mb-8">{post.attributes.author}</p>
                 <img src={post.attributes.image}/>
-                <article className="prose ">
+                <article className="prose prose-blog-post">
                     <ReactMarkdown remarkPlugins={[remarkGfm]}>{post.body}</ReactMarkdown>
                 </article>
             </div>
--- tc-marketplace/frontend/src/pages/BlogPostPage.jsx
+++ tc-marketplace/frontend/src/pages/BlogPostPage.jsx
@@ -1,10 +1,12 @@
 import { useParams } from "react-router-dom";
 import matter from "gray-matter";
 import BlogPost from "../components/BlogPost";
+import Navbar from "../components/Navbar";
 
 export default function BlogPostPage() {
     return (
-        <div>
+        <div className="bg-slate-50">
+            <Navbar theme="light" />
             <BlogPost slug={'setting-up-your-first-tc-lab'}/>
         </div>
     )
