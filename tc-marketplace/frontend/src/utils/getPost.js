c7d550444940a54aeee8509a5598a2269aa34b39
Roman <roman.blumovich@gmail.com>
2025-10-19 16:58:36 +0300

    cleaned

--- tc-marketplace/frontend/src/utils/getPost.js
+++ tc-marketplace/frontend/src/utils/getPost.js
@@ -1,9 +1,7 @@
-import matter from 'gray-matter';
 
 export default async function getPost(slug) {
   const response = await fetch(`/content/blog/${slug}.md`);
   const text = await response.text();
-//   const { data, content } = matter(text);
   return { text };
-return text
+
 }
