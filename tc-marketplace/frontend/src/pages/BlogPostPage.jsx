123221b37353894fc52d8225e9bbeb0833da5455
Roman <roman.blumovich@gmail.com>
2025-10-19 17:05:54 +0300

    load post from slug and navigate to /blog if post not found

--- tc-marketplace/frontend/src/pages/BlogPostPage.jsx
+++ tc-marketplace/frontend/src/pages/BlogPostPage.jsx
@@ -1,13 +1,22 @@
-import { useParams } from "react-router-dom";
-import matter from "gray-matter";
+import { useParams, useNavigate } from "react-router-dom";
 import BlogPost from "../components/BlogPost";
 import Navbar from "../components/Navbar";
+import { blogPosts } from "../data/blogPosts";
+import { useEffect } from "react";
 
 export default function BlogPostPage() {
+    const { slug } = useParams();
+    const navigate = useNavigate();
+    const post = blogPosts.find(post => post.slug === slug)
+    
+    useEffect(() => {
+        if(!post ) navigate('/blog', { replace: true });
+    })
+
     return (
         <div className="bg-slate-50">
             <Navbar theme="light" />
-            <BlogPost slug={'setting-up-your-first-tc-lab'}/>
+            <BlogPost slug={slug}/>
         </div>
     )
     
