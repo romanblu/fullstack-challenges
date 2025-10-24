3b6f5eb355e3cf5dae2f0233ad55abd557d4dda1
Roman <roman.blumovich@gmail.com>
2025-10-24 19:58:34 +0300

    refactored controller, added error handling

--- tc-marketplace/backend/controllers/productController.js
+++ tc-marketplace/backend/controllers/productController.js
@@ -1,28 +1,91 @@
+import mongoose from 'mongoose';
 import Product from '../models/Product.js';
 
+// @desc    Get all products
+// @route   GET /api/products
+// @access  Public
 export const listProducts = async (req, res) => {
   const q = {};
+
   if (req.query.q) q.title = { $regex: req.query.q, $options: 'i' };
-  const products = await Product.find(q).limit(100);
+
+  const products = await Product.find(q).limit(100).populate('seller', 'name');
   res.json(products);
 };
+
+// @desc    Get single product
+// @route   GET /api/products/(:id || :slug)
+// @access  Public
 export const getProduct = async (req, res) => {
-  const p = await Product.findOne({ id: req.params._id }).populate('seller', 'name');
-  if (!p)  return res.status(404).json({ message: 'Not found' });
+  const { id } = req.params
+  let p = {}
+
+  // find by ID or slug
+  p = await Product.findOne({ slug: id }).populate('seller', 'name')
+
+  if(!p && mongoose.Types.ObjectId.isValid(id)){
+    p = await Product.findById(id).populate('seller', 'name')
+  }
+
+  if (!p)  {
+    res.status(404)
+    throw Error('Product not found')
+  }
+  res.status(200)
   res.json(p);
 };
 
+
+// @desc    Create product
+// @route   POST /api/products
+// @access  Private - seller/admin
 export const createProduct = async (req, res) => {
   const data = req.body;
+
+  if(!data.price || !data.title){
+    res.status(400)
+    throw new Error('Title and price required')
+  }
+
   data.seller = req.user.id; // from protect middleware
+
   const product = await Product.create(data);
-  res.json(product);
+  res.status(201).json(product);
 };
+
+
+// @desc    Update single product
+// @route   PUT /api/products/:id
+// @access  Private - seller/admin
 export const updateProduct = async (req, res) => {
-  const updated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  res.json(updated);
+  try{
+    const updated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  }catch(err){
+    res.status(500)
+    throw new Error('Could not update product. ', err)
+  }
 };
+
+
+// @desc    delete product
+// @route   DELETE /api/products/:id
+// @access  Private - seller/admin
 export const deleteProduct = async (req, res) => {
-  await Product.findByIdAndDelete(req.params.id);
-  res.json({ ok: true });
+  try{ 
+
+    // TODO: validation for seller and user 
+
+    const p = await Product.findByIdAndDelete(req.params.id);
+    
+    if(!p){
+      res.status(404)
+      throw new Error('Could not find product')
+    }
+
+    res.json({ "message": "deleted successfully" });
+  }catch(err){
+    res.status(500)
+    throw new Error('Could not delete product. Error: ', err)
+  }
 };
