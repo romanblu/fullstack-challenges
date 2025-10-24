53a770db2b0ea36aabfc77287988ea093c1f0e10
Roman <roman.blumovich@gmail.com>
2025-10-24 14:21:07 +0300

    added error middleware

---
+++ tc-marketplace/backend/middleware/errorMiddleware.js
@@ -0,0 +1,17 @@
+
+export const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+} 
+
+export const errorHandler = (err, req, res, next) => {
+    // Sometimes Express sets statusCode = 200 even on errors, fix it:
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+    res.status(statusCode).json({
+    message: err.message || 'Internal Server Error',
+    // show stack trace only in development for security
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+}
--- tc-marketplace/backend/server.js
+++ tc-marketplace/backend/server.js
@@ -8,6 +8,7 @@ import productRoutes from "./routes/products.js";
 import orderRoutes from "./routes/orders.js";
 import authRoutes from "./routes/auth.js";
 import blogRoutes from "./routes/blog.js";
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -22,6 +23,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/blog", blogRoutes);
 app.use("/api/auth", authRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
