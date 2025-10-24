671f913fb0316c67488cc3cb22110d1944fc77db
Roman <roman.blumovich@gmail.com>
2025-10-24 20:12:06 +0300

    refactored

--- tc-marketplace/backend/controllers/authController.js
+++ tc-marketplace/backend/controllers/authController.js
@@ -1,23 +1,56 @@
 import User from '../models/User.js';
 import { hash, compare, signToken } from '../utils/auth.js';
 
+// @desc    Register new user
+// @route   POST /api/auth/register
+// @access  Public
 export const register = async (req, res) => {
   const { name, email, password, role } = req.body;
-  if (!email || !password) return res.status(400).json({ message: 'Missing' });
+
+  if (!email) {
+    res.status(400)
+    throw new Error('Missing email')
+  }
+  
+  if (!password) {
+    res.status(400)
+    throw new Error('Missing password')
+  }
+
   const existing = await User.findOne({ email });
-  if (existing) return res.status(400).json({ message: 'Email exists' });
-  const pw = await hash(password);
-  const user = await User.create({ name, email, password: pw, role });
+
+  if (existing) {
+    res.status(400)
+    throw new Error('Email already exists')
+  }
+
+  const hashedPw = await hash(password);
+  const user = await User.create({ name, email, password: hashedPw, role });
   const token = signToken({ id: user._id, role: user.role });
+
   res.json({ user: { id: user._id, email: user.email, name: user.name, role: user.role }, token });
 };
 
+// @desc    Login user
+// @route   POST /api/auth/login
+// @access  Public
 export const login = async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
-  if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+
+  if (!user) {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
+
   const ok = await compare(password, user.password);
-  if (!ok) return res.status(401).json({ message: 'Invalid credentials' });
+
+  if (!ok)  {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
+
   const token = signToken({ id: user._id, role: user.role });
-  res.json({ token, user: { id: user._id, email: user.email, name: user.name, role: user.role } });
+  res.status(200).json({ token, user: { id: user._id, email: user.email, name: user.name, role: user.role } });
 };
