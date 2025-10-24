c1cda298dd9aec0201621fc5c1022d9e666ccd5b
Roman <roman.blumovich@gmail.com>
2025-10-24 15:41:04 +0300

    refactored functions, added API documentation, error handling

--- tc-marketplace/backend/controllers/blogController.js
+++ tc-marketplace/backend/controllers/blogController.js
@@ -1,37 +1,91 @@
+import slugify from 'slugify';
 import BlogPost from '../models/BlogPost.js';
 
+// @desc    Get all blog posts
+// @route   GET /api/blog
+// @access  Public
 export const listBlogPosts = async (req, res) => {
   const q = {};
   if (req.query.q) q.title = { $regex: req.query.q, $options: 'i' };
-  const blogPosts = await BlogPost.find(q).limit(100);
+  const blogPosts = await BlogPost.find(q).limit(100).sort({ createdAt: -1 }).populate('author', 'name email');
   res.json(blogPosts);
 };
 
+// @desc    Get single blog post by slug
+// @route   GET /api/blog/:slug
+// @access  Public
 export const getBlogPost = async (req, res) => {
-  const p = await BlogPost.findOne({ slug: req.params.slug }).populate('author', 'author name');
-  if (!p)  return res.status(404).json({ message: 'Not found' });
+  const p = await BlogPost.findOne({ slug: req.params.slug }).populate('author', 'name email');
+  if (!p) { 
+    res.status(404)
+    throw new Error('Blog post not found')
+  }
+
   res.json(p);
 };
 
+// @desc    Create blog post
+// @route   POST /api/blog
+// @access  Private - author/admin
 export const createBlogPost = async (req, res) => {
-  const data = req.body;
-  data.author = req.user.id; // from protect middleware
-  const post = await BlogPost.create(data);
+  const { title, content, imageUrl} = req.body;
+  if(!content || !title){
+    res.status(400)
+    throw new Error('Title and content are required')
+  }
+  
+  const post = await BlogPost.create({
+    title,
+    content,
+    imageUrl,
+    slug: slugify(title),
+    author: req.user._id
+  });
+
   res.json(post);
 };
 
+// @desc    Update blog post
+// @route   PUT /api/blog/:slug
+// @access  Private - author/admin
 export const updateBlogPost = async (req, res) => {
-  const updated = await BlogPost.findOneAndUpdate({slug: req.params.slug}, req.body, { new: true });
-  if (!updated) {
-    return res.status(404).json({ message: 'Blog post not found' });
+  const { slug } = req.params
+  const post = await BlogPost.findOne({ slug })
+
+  if(!post){
+    res.status(404)
+    throw new Error('Blog post was not found')
   }
+  console.log(req.user)
+  if(post.author.toString() !== req.user.id.toString()){
+    res.status(403)
+    throw new Error('Only the author can edit this post')
+  }
+
+  Object.assign(post, req.body)
+  const updated = await post.save()
+
   res.json(updated);
 };
 
+// @desc    delete a blog post
+// @route   DELETE /api/blog/:slug
+// @access  Private - author/admin
 export const deleteBlogPost = async (req, res) => {
-  const result = await BlogPost.findOneAndDelete(req.params.slug);
-  if(!result) {
-    return res.status(404).json({ message: 'Blog post not found' });
+  const { slug } = req.params
+
+  const post = await BlogPost.findOne({slug})
+  if(!post) {
+    res.status(404)
+    throw new Error('Blog post not found')
   }
-  res.json({ ok: true });
+
+  
+  if(post.author.toString() !== req.user.id) {
+    res.status(403)
+    throw new Error('Not authorized to delete the post')
+  }
+
+  await BlogPost.deleteOne(post);
+  res.json({ message: "Post deleted successfull" });
 };
