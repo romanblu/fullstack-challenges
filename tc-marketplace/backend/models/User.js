0cc4bcf71bc65588bbf5692a0208a15cb72c53e2
Roman <roman.blumovich@gmail.com>
2025-10-26 10:33:02 +0200

    updated values for store owner

--- tc-marketplace/backend/models/User.js
+++ tc-marketplace/backend/models/User.js
@@ -5,7 +5,19 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   phone: { type: String },
   role: { type: String, enum: ['buyer','seller','admin'], default: 'buyer' },
+  
+  
+   // Seller-only fields
+  storeName: { type: String, required: function() { return this.role === "seller"; } },
+  contactEmail: { type: String },
+  contactPhone: { type: String },
+  location: {type: String }, 
+  profilePicture: {type: String },  
+  description: { type: String },
+  storeSlug: { type: String },
+  category: { type: String },
   verified: { type: Boolean, default: false }, // seller verification
+
   createdAt: { type: Date, default: Date.now }
 });
 export default mongoose.model('User', userSchema);
