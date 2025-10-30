2568415ee26442b901bbf86114bc09ff6a9e1a79
Roman <roman.blumovich@gmail.com>
2025-10-30 10:28:01 +0200

    fix blog card ui bugs, integrated into blog dashboard

--- tc-marketplace/frontend/src/components/BlogCard.jsx
+++ tc-marketplace/frontend/src/components/BlogCard.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 export default function BlogCard({ post }) {
 
     return (
-        <div className="bg-white rounded-xl shadow hover:shadow-lg transition flex flex-col">
+        <div className="bg-white rounded-xl shadow hover:shadow-lg transition flex flex-col h-full">
             <img src={post.image} alt={post.title} className="h-48 w-full object-cover rounded-t-xl" />
             <div className="p-4 flex flex-col flex-grow">
                 <h3 className="text-xl font-semibold text-green-900 mb-2">{post.title}</h3>
                 <p className="text-gray-700 text-sm flex-grow">{post.excerpt}</p>
                 <div className="mt-4 flex justify-between text-sm text-gray-500">
-                    <span>{post.author}</span>
+                    <span>{post.author.name}</span>
                     <span>{new Date(post.date).toLocaleDateString()}</span>
                 </div>
                 <Link
--- tc-marketplace/frontend/src/components/BlogDashboard.jsx
+++ tc-marketplace/frontend/src/components/BlogDashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchMyPosts } from "../api/blog";
+import BlogCard from "./BlogCard";
 
 const BlogDashboard = ({ setActiveTab }) => {
     const [blogPosts, setBlogPosts] =  useState([]);
@@ -16,15 +17,30 @@ const BlogDashboard = ({ setActiveTab }) => {
     const handleCreateBlogPost = () => {
         setActiveTab("newBlogPost");
     }
-
+    console.log("Blog posts:", blogPosts);
     return (
-        <div className="mx-auto   bg-slate-100 text-center p-6">
+        <div className="mx-auto   bg-slate-100 text-center p-6 container max-w-[1100px]">
             <div className="mb-4">
                 <h1 className="text-xl pb-3">Blog Post List</h1>
                 <p>Here you can create and manage your blog posts.</p>
             </div>
-            <button onClick={handleCreateBlogPost} className="bg-lime-500 hover:bg-lime-600 text-green-950 font-semibold py-2 px-2 rounded-md shadow-md">Create Blog Post</button>
-            {blogPosts.length === 0 && <h1 className="text-xl py-3" >Nothing to show yet...</h1>}
+            <button onClick={handleCreateBlogPost} className="bg-lime-500 hover:bg-lime-600 text-green-950 font-semibold py-2 px-4 rounded-md shadow-md transition">Create Blog Post</button>
+            {
+            blogPosts.length === 0 ? 
+            <h1 className="text-xl py-3" >Nothing to show yet...</h1> 
+            :
+            <div className="mt-6 ">
+                <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+                    {
+                        blogPosts.length !== 0 && blogPosts.map((post) => (
+                            <BlogCard post={post} />
+                        ))
+                    }
+                </div>
+            </div>                
+            
+            }
+
         </div>    
     )
 }
