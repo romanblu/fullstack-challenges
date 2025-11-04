62e3f483b85c21342ce8f15bf6dd3f01865687f1
Roman <roman.blumovich@gmail.com>
2025-11-05 00:10:35 +0200

    added route for seller products

--- tc-marketplace/backend/routes/products.js
+++ tc-marketplace/backend/routes/products.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { protect, requireRole } from '../middleware/auth.js';
-import { listProducts, getProduct, createProduct, updateProduct, deleteProduct, getFeaturedProducts } from '../controllers/productController.js';
+import { listProducts, getProduct, createProduct, updateProduct, deleteProduct, getFeaturedProducts, getSellerProducts } from '../controllers/productController.js';
 
 const router = express.Router();
 router.get('/', listProducts);
 router.get('/featured', getFeaturedProducts);
+router.get('/seller/:id', getSellerProducts);
 router.get('/:id', getProduct);
 router.post('/', protect, requireRole('seller'), createProduct);
 router.put('/:id', protect, requireRole('seller'), updateProduct);
