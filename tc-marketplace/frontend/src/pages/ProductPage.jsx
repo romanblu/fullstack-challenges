d922b3eeaaa175eec1c09bdb13e5b88a45933d34
Roman <roman.blumovich@gmail.com>
2025-11-05 21:34:09 +0200

    connected to backend, added categories selector

--- tc-marketplace/frontend/src/pages/ProductPage.jsx
+++ tc-marketplace/frontend/src/pages/ProductPage.jsx
@@ -1,19 +1,44 @@
-
+import { useQuery } from '@tanstack/react-query';
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getProductBySlug } from "../api/product";
+import CategorySelector from "../components/ui/CategorySelector";
+import { getCategoryTree } from '../api/category';
+import SelectedCategories from '../components/ui/SelectedCategories';
 
-const ProductPage = () => {
-  const product = {
-    id: 1,
-    name: "Philodendron Gloriosum",
-    description:
-      "A premium tissue culture plant known for its velvety leaves and striking white veins. Ideal for home growers and collectors.",
-    price: 19.99,
-    stock: "Available",
-    image:
-      "https://images.unsplash.com/photo-1611527664689-d430dd2a6774?auto=format&fit=crop&w=800&q=80",
-    category: "House Plant",
-  };
+const ProductPage = () => { 
+  const { slug } = useParams()
+  const [product, setProduct] = useState(null)
+  
+ const { data: categoryTree, isLoadingCategoryTree, categoryError } = useQuery({
+    queryKey: ["categories"],
+    queryFn: () =>
+    getCategoryTree().then((res) =>  res.data),
+  });
+
+  const {
+    data: productData,
+    isLoading: isProductLoading,
+    error: productError
+  } = useQuery({
+    queryKey: ["product", slug],
+    queryFn: () => getProductBySlug(slug).then((res) => res.data),
+    enabled: !!slug,  // only fetch after slug exists
+  }); 
+
+  useEffect( () =>{
+    if(productData){
+      setProduct(productData)
+    }
+  }, [productData])
+
+  if (!product) return <p className="text-center mt-10">Loading...</p>;
+
+  if(isLoadingCategoryTree) return <p>Loading category tree...</p>
+
+  if(categoryError) console.log("Category error", categoryError)
 
   return (
     <>
@@ -21,10 +46,11 @@ const ProductPage = () => {
       <section className="bg-slate-50 py-10">
         <div className="max-w-5xl mx-auto px-6 grid grid-cols-1 md:grid-cols-2 gap-10">
           {/* Product Image */}
+          {/* TODO : rotating image gallery */}
           <div className="rounded-xl overflow-hidden shadow-md">
             <img
-              src={product.image}
-              alt={product.name}
+              src={product?.mainImage}
+              alt={product?.name}
               className="w-full h-full object-cover"
             />
           </div>
@@ -33,23 +59,34 @@ const ProductPage = () => {
           <div className="flex flex-col justify-between">
             <div>
               <h1 className="text-3xl font-semibold text-gray-800 mb-2">
-                {product.name}
+                {product?.name}
               </h1>
+              {/* Categories */}
+
+              {
+                // <CategorySelector 
+                //   categoryTree={categoryTree}
+                //   selectedIds={product.categories}
+                //   onChange={{}}
+                //   />
+                <SelectedCategories selectedIds={product.categories} />
+              }
+
               <p className="text-green-700 text-sm uppercase mb-3">
-                {product.category}
+                {product?.category}
               </p>
-              <p className="text-gray-600 mb-6">{product.description}</p>
+              <p className="text-gray-600 mb-6">{product?.description}</p>
               <p className="text-2xl font-bold text-green-800 mb-4">
-                ${product.price}
+                ${product?.price}
               </p>
               <p
                 className={`mb-4 ${
-                  product.stock === "Available"
+                  product?.stock === "Available"
                     ? "text-green-600"
                     : "text-red-500"
                 }`}
               >
-                {product.stock}
+                {product?.stock}
               </p>
               <button className="bg-green-700 text-white px-6 py-2 rounded-lg hover:bg-green-800 transition">
                 Add to Cart
@@ -59,33 +96,7 @@ const ProductPage = () => {
         </div>
       </section>
 
-      {/* Related Products */}
-      <section className="max-w-6xl mx-auto px-6 mt-12 mb-20">
-        <h2 className="text-2xl font-semibold text-gray-800 mb-6">
-          Related Products
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          {[1, 2, 3].map((_, i) => (
-            <div
-              key={i}
-              className="bg-white shadow rounded-lg overflow-hidden hover:shadow-lg transition"
-            >
-              <img
-                src={`https://source.unsplash.com/400x400/?plant,${i}`}
-                alt="Related Plant"
-                className="w-full h-48 object-cover"
-              />
-              <div className="p-4">
-                <h3 className="font-semibold text-gray-700 mb-2">
-                  Related Plant {i + 1}
-                </h3>
-                <p className="text-green-700 font-semibold">$14.99</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-
+      
       <Footer />
     </>
   );
