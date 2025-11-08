97198f00d69c9b44ad4f835d7024e1c4a4915e09
Roman <roman.blumovich@gmail.com>
2025-11-08 21:46:01 +0200

    refactored product controller into product service

--- tc-marketplace/backend/modules/product/product.contoller.js
+++ tc-marketplace/backend/modules/product/product.contoller.js
@@ -2,86 +2,49 @@ import mongoose from 'mongoose';
 import Product from './product.model.js';
 import Variant from '../variant/variant.model.js';
 import asyncHandler from '../../utils/asyncHandler.js'
+import * as productService from './product.service.js'
+import ApiError from '../../utils/ApiError.js'
 
 // @desc    Get all products
 // @route   GET /api/products
 // @access  Public
-export const listProducts = async (req, res) => {
-  const q = {};
-
-  if (req.query.q) q.title = { $regex: req.query.q, $options: 'i' };
-
-  const products = await Product.find(q).limit(100).populate('seller', 'name');
-  res.json(products);
-};
+export const listProducts = asyncHandler(async (req, res) => {
+  const products = await productService.listProducts(req.query)
+  res.json({products})
+});
 
 // @desc    Get single product
 // @route   GET /api/products/(:id || :slug)
 // @access  Public
-export const getProduct = asyncHandler(async (req, res) => {
-  const { id } = req.params
-  let p = {}
-
-  // find by ID or slug
-  p = await Product.findOne({ slug: id }).populate('seller', 'name').populate("categories", "_id name slug").populate("store", "name contactEmail contactPhone location");
-
-  if(!p && mongoose.Types.ObjectId.isValid(id)){
-    p = await Product.findById(id).populate('seller', 'name').populate("categories", "_id name slug").populate("store", "name contactEmail contactPhone location");
-  }
-
-  if (!p)  {
-    res.status(404)
-    throw Error('Product not found')
-  }
-  res.status(200)
-  res.json(p);
+export const getProduct = asyncHandler(async (req, res) => { 
+  const product = await productService.getProduct(req.params.id)
+  if(!product) throw ApiError.notFound("Product not found")
+  res.status(200).json({product})
 });
 
 // @desc    Get all products by seller
 // @route   GET /api/products/seller/:id 
 // @access  Public
 export const getSellerProducts = async (req, res) => {
-  
-  try{
-    const { id } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({ message: "Invalid seller ID" });
-    }
-  
-    const products = await Product.find({ seller: id })
-      .populate("seller", "name email")   // adjust fields as needed
-      .populate("categories", "_id name slug");
-
-    res.status(200).json(products);
-  
-  }catch (err) {
-    console.error("Error fetching seller products:", err);
-    res.status(500).json({ message: "Server error" });
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    throw ApiError.badRequest(400, "Invalid seller ID")
   }
-  
+
+  const products = await productService.getSellerProducts(id)
+  res.json(products);
 };
 
 // @desc    Get featured products
 // @route   GET /api/product/featured
 // @access  Public
 export const getFeaturedProducts = async (req, res) => {
-  try{
-    const products = await Product.find({ 
-      featured: true
-    }).sort({ createdAt: -1})
-    .limit(8)
-    .select("name price slug image description seller")
-    .populate('seller', 'name');
-    
-    if (!products) { 
-      res.status(404)
-      throw new Error('Product not found')
-    }
-    
-    res.json(products);
-  } catch (err){
-    res.status(500).json({ message: "Failed getting featured products", error: err.message})
+  const products = productService.getFeaturedProducts()
+  if (!products) { 
+    throw ApiError.internal("Failed to get featured products")
   }
+  
+  res.json(products);  
 };
 
 
@@ -89,68 +52,47 @@ export const getFeaturedProducts = async (req, res) => {
 // @route   POST /api/products
 // @access  Private - seller/admin
 export const createProduct = async (req, res) => {
-  try{
-    const {
-      name,
-      slug,
-      species,
-      description,
-      price,
-      quantity,
-      mainImage,
-      productType,
-      categories,
-      store,
-      variants,
-    } = req.body;
-    
-    if(!price || !name){
-      res.status(400)
-      throw new Error('Title and price required')
-    }
-    
-    const seller = req.user.id; // from protect middleware
-    
-    const product = new Product({
-      name,
-      slug,
-      species,
-      description,
-      price,
-      quantity,
-      mainImage,
-      seller,
-      store,
-      productType,
-      categories,
-    });
-
-    product.save()
-    
-    let createdVariantIds =[]
-    
-    if (Array.isArray(variants) && variants.length > 0) {
-      for (let v of variants) {
-        const created = await Variant.create({
-          ...v,
-          product: product._id
-        });
-        createdVariantIds.push(created._id);
-      }
-      product.variants = createdVariantIds;
-      await product.save();
-    }
-
-
-    const populatedProduct = await Product.findById(product._id)
-      .populate("categories", "_id name slug")
-      .populate("variants");
-
-    res.status(201).json(populatedProduct);
-  } catch (err) {
-    console.error("Error creating product:", err);
-    res.status(500).json({ message: "Server error",err });
+  const {
+    name,
+    slug,
+    species,
+    description,
+    price,
+    quantity,
+    mainImage,
+    productType,
+    categories,
+    store,
+    variants,
+  } = req.body;
+  
+  if(!price || !name){
+    throw ApiError.badRequest('Title and price required')
   }
+  
+  const seller = req.user.id; // from protect middleware
+  
+  const product = productService.createProduct({
+    name,
+    slug,
+    species,
+    description,
+    price,
+    quantity,
+    mainImage,
+    seller,
+    store,
+    productType,
+    categories,
+    store,
+    variants
+  });
+
+  if(!product){
+    throw ApiError.internal("Error creating a product")
+  }
+
+  res.json(product);
 };
 
 
@@ -158,13 +100,13 @@ export const createProduct = async (req, res) => {
 // @route   PUT /api/products/:id
 // @access  Private - seller/admin
 export const updateProduct = async (req, res) => {
-  try{
-    const updated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(updated);
-  }catch(err){
-    res.status(500)
-    throw new Error('Could not update product. ', err)
-  }
+    const updated = await productService.updateProduct(req.params.id, req.body);
+
+    if(!updated){
+      throw ApiError.internal("Could not update product")
+    }
+
+    res.json(updated);    
 };
 
 
@@ -172,20 +114,14 @@ export const updateProduct = async (req, res) => {
 // @route   DELETE /api/products/:id
 // @access  Private - seller/admin
 export const deleteProduct = async (req, res) => {
-  try{ 
-
+  
     // TODO: validation for seller and user 
-
     const p = await Product.findByIdAndDelete(req.params.id);
     
     if(!p){
-      res.status(404)
-      throw new Error('Could not find product')
+      throw ApiError.notFound("Could not find product")
     }
-    res.status(204)
-    res.json({ "message": "deleted successfully" });
-  }catch(err){
-    res.status(500)
-    throw new Error('Could not delete product. Error: ', err)
-  }
+    
+    res.status(204).json({message: "Deleted successfuly"})
+ 
 };
---
+++ tc-marketplace/backend/modules/product/product.service.js
@@ -0,0 +1,122 @@
+import Product from './product.model.js'
+import Variant from '../variant/variant.model.js'
+import mongoose from 'mongoose';
+export const listProducts = async (filters) => {
+    const query = {}
+
+    if (filters?.title) query.title = { $regex: filters.title, $options: 'i' };
+    
+    if (filters?.category) {
+        query.categories = filters.category;
+    }
+
+    if (filters?.seller) {
+        query.seller = filters.seller;
+    }
+    const products = await Product.find(query).limit(100).populate('seller', 'name');
+    return products
+}
+
+export const getProduct = async (id) => {
+    let p = {}
+
+    // find by ID or slug
+    p = await Product.findOne({ slug: id }).populate('seller', 'name').populate("categories", "_id name slug").populate("store", "name contactEmail contactPhone location");
+
+    if(!p && mongoose.Types.ObjectId.isValid(id)){
+        p = await Product.findById(id).populate('seller', 'name').populate("categories", "_id name slug").populate("store", "name contactEmail contactPhone location");
+    }
+
+    return p
+}
+
+export const getSellerProducts = async (id) => {
+
+    const products = await Product.find({ seller: id })
+        .populate("seller", "name email")   // adjust fields as needed
+        .populate("categories", "_id name slug");
+
+    return products;  
+};
+
+export const getFeaturedProducts = async () => {
+
+    const products = await Product.find({ 
+      featured: true
+    }).sort({ createdAt: -1})
+    .limit(8)
+    .select("name price slug image description seller")
+    .populate('seller', 'name');
+
+    return products
+};
+
+
+export const createProduct = async (body) => {
+    const {
+      name,
+      slug,
+      species,
+      description,
+      price,
+      quantity,
+      mainImage,
+      seller,
+      productType,
+      categories,
+      store,
+      variants,
+    } = body;
+    
+   
+    
+    const product = new Product({
+      name,
+      slug,
+      species,
+      description,
+      price,
+      quantity,
+      mainImage,
+      seller,
+      store,
+      productType,
+      categories,
+      store,
+      variants
+    });
+
+    product.save()
+    
+    let createdVariantIds =[]
+    
+    if (Array.isArray(variants) && variants.length > 0) {
+      for (let v of variants) {
+        const created = await Variant.create({
+          ...v,
+          product: product._id
+        });
+        createdVariantIds.push(created._id);
+      }
+      product.variants = createdVariantIds;
+      await product.save();
+    }
+
+
+    const populatedProduct = await Product.findById(product._id)
+      .populate("categories", "_id name slug")
+      .populate("variants");
+
+    return populatedProduct;
+  
+};
+
+export const updateProduct = async (id, body) => {
+    const updated = await Product.findByIdAndUpdate(id, body, { new: true });
+    return updated
+};
+
+export const deleteProduct = async (id) => {
+    const p = await Product.findByIdAndDelete(id);
+    return p
+}
