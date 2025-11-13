ac20e907f85aa6910be55d2a5adb9559028213c9
Roman <roman.blumovich@gmail.com>
2025-11-13 14:45:15 +0200

    refactor login page

---
+++ tc-marketplace/frontend/src/hooks/useAuth.jsx
@@ -0,0 +1,31 @@
+import { useContext, useState } from "react"
+import { AuthContext } from '../context/AuthContext'
+import { loginUser } from "../api/auth"
+
+export const useAuth = () => {
+    const { setUser } = useContext(AuthContext)
+    const [loading, setLoading ] = useState(false)
+    const [error, setError] = useState("")
+
+    const login = async (formData) => {
+
+        setLoading(true)
+        setError("")
+
+        try{
+            const data = await loginUser(formData)
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("user", JSON.stringify(data.user));
+            setUser(data.user)
+            return data.user
+        }catch(err){
+            console.error(err)
+            setError("Login failed", err)
+        }finally{
+            setLoading(false)
+        }
+     
+    }
+    
+    return { login, loading, error}
+}
--- tc-marketplace/frontend/src/pages/SignIn.jsx
+++ tc-marketplace/frontend/src/pages/SignIn.jsx
@@ -4,34 +4,25 @@ import { useNavigate } from "react-router-dom";
 import { loginUser } from "../api/auth";
 import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
+import { useAuth } from "../hooks/useAuth";
 
 const SignIn = () => {
     const [formData, setFormData] = useState({ email: "", password: "" });
-    const [error, setError] = useState("");
-
-    const { user, setUser, logout } = useContext(AuthContext)
-    
+    const { login, loading, error} = useAuth()
+   
 
     const navigate = useNavigate();
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     }
-
     const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        try{
-            const data = await loginUser(formData)  
-            localStorage.setItem("token", data.token);
-            localStorage.setItem("user", JSON.stringify(data.user));
-            setUser(data.user)
-            navigate('/')
-        }catch(err) {
-            console.log(err)
-        }
+        e.preventDefault()
+        const user = await login(formData)
+        console.log(user)
+        if(user) navigate("/")
     }
-
+    
     return (
         <section className=" bg-gradient-to-br from-green-600 via-green-800 to-green-950 text-green-50 px-4">
                 <Navbar />
@@ -39,7 +30,7 @@ const SignIn = () => {
                     <div className="bg-white text-green-900 rounded-xl shadow-lg p-8 w-full max-w-md ">
                         <h1 className="text-3xl font-bold text-center mb-6">Sign In</h1>
 
-                        {error && <p className="text-red-600 text-sm text-center mb-4">{error}</p>}
+                        {/* {error && <p className="text-red-600 text-sm text-center mb-4">{error}</p>} */}
 
                         <form onSubmit={handleSubmit} className="space-y-5">
                         <div>
@@ -70,7 +61,7 @@ const SignIn = () => {
                             type="submit"
                             className="w-full bg-green-700 text-white py-2 rounded-md hover:bg-green-800 transition"
                         >
-                            Sign In
+                           {loading ? 'Signing in...' : 'Sign In'}
                         </button>
                         </form>
 
