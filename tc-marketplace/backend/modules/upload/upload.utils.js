1da5d1c89f294601bffecdf423a89ea125595b09
Roman <roman.blumovich@gmail.com>
2025-12-08 16:34:43 +0200

    added endpoint to s3 config and installed package

--- tc-marketplace/backend/modules/upload/upload.utils.js
+++ tc-marketplace/backend/modules/upload/upload.utils.js
@@ -7,6 +7,8 @@ dotenv.config();
 
 export const s3 = new S3Client({
   region: process.env.AWS_REGION,
+  endpoint:`https://s3.${process.env.AWS_REGION}.amazonaws.com`,
+   forcePathStyle: false,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
