2c41c75ebf09a06b64d4b1621a0668482bb245e1
Roman <roman.blumovich@gmail.com>
2026-01-25 14:56:06 +0200

    added ui effect for image upload statuses

--- tc-marketplace/frontend/src/features/ImageUploader/ImageItem.jsx
+++ tc-marketplace/frontend/src/features/ImageUploader/ImageItem.jsx
@@ -2,8 +2,9 @@ import { MdDelete } from "react-icons/md";
 
 
 export default function ImageItem({ image, onDelete }) {
+
     return (
-        <div className="relative w-full h-32 overflow-hidden rounded border">
+        <div className={`relative w-full h-32 overflow-hidden rounded border-3 ${image.uploadStatus === 'uploading' ? 'border-yellow-500 animate-pulse' : image.uploadStatus === 'uploaded' ? 'border-green-500' : 'border-gray-300' ? image.uploadStatus === 'error' : 'border-red-600' } cursor-pointer`}>
             <img src={image.previewUrl} className="object-cover w-full h-full" />
             <MdDelete size={28} color="red" className=" absolute top-1 right-1 opacity-60 hover:opacity-100 transition cursor-pointer" onClick={()=> onDelete(image)}/>
         </div>
