1674e3ac542321d135f8266ebe137b498b94b81d
Roman <roman.blumovich@gmail.com>
2026-01-28 17:37:12 +0200

    cart routes

--- tc-marketplace/backend/modules/cart/cart.routes.js
+++ tc-marketplace/backend/modules/cart/cart.routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import {addCartItem, updateCart, deleteCartItem, cartCheckout} from './cart.controller.js';
 
 const router = express.Router();
 
 router.post('/add', addCartItem);
 router.patch('/update', updateCart);
 router.delete('/delete', deleteCartItem);
-router.post('/checkout', checkoutCart);
+router.post('/checkout', cartCheckout);
+
+export default router
