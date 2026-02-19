4609264c68573c856aa74b3abaa3ff793c69335a
Roman <roman.blumovich@gmail.com>
2026-02-19 11:57:40 +0200

    integrated add to cart to product card

--- tc-marketplace/frontend/src/features/products/ProductCard.jsx
+++ tc-marketplace/frontend/src/features/products/ProductCard.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { Link } from "react-router-dom";
+import AddToCartButton from "../cart/AddToCartButton";
 
 export default function ProductCard({ product, setSelectedProduct, setActiveTab }) {
     const { user } = useContext(AuthContext);
@@ -24,7 +25,7 @@ export default function ProductCard({ product, setSelectedProduct, setActiveTab 
             <Link to={`/product/${product.slug}`} className="text-lg font-semibold text-green-900 px-2">{product.name}</Link>
             <p className="text-sm text-gray-600 mb-2 px-2">{product.excerpt}</p>
             <span className="text-orange-800 font-light text-sm px-2">{product.quantity < 15 ? `only ${product.quantity} left` : ''}</span>
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center py-2 px-2">
                 <span className="text-green-700 font-bold px-2">{`$ ${product.price}`}</span>
                 
                 {isSeller ? setSelectedProduct ? 
@@ -32,9 +33,11 @@ export default function ProductCard({ product, setSelectedProduct, setActiveTab 
                     Edit Product
                 </button>
                 :  '' : // setSelectedProduct check to avoid showing Add To Cart button for the sellers' items in marketplace and also update item outside of dashboard
-                <button className="bg-green-600 text-white px-2 py-1 rounded-md hover:bg-green-700 transition m-3">
-                Add to Cart
-                </button> }
+                
+                <AddToCartButton productId={product._id} />
+
+                }
+
             </div>
         </div>
     );
