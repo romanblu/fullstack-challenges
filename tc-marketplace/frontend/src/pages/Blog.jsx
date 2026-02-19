d76fcfe8cd347b3e630b7fb8c16931e4c7bbafd9
Roman <roman.blumovich@gmail.com>
2026-02-19 17:49:53 +0200

    fixed ui bug

--- tc-marketplace/frontend/src/pages/Blog.jsx
+++ tc-marketplace/frontend/src/pages/Blog.jsx
@@ -13,9 +13,9 @@ const Blog = () => {
     if(error) return <ErrorMessage message="Error getting blog posts data"/>
     
     return (
-    <section className="bg-slate-50  px-6 py-12 text-green-950 min-h-screen">
+    <section className="bg-slate-50  px-6 text-green-950 min-h-screen">
         <Navbar theme="light" />
-        <div className="container max-w-[1100px] mx-auto pt-8">
+        <div className="container max-w-[1100px] mx-auto py-12">
             <h1 className="text-3xl font-bold text-center mb-2.5">Our Blog</h1>
             <p className="text-center text-[20px]">Grow your knowledge — discover tips, techniques, and stories from experienced tissue culture enthusiasts.</p>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
