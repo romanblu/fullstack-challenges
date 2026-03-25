29bd93211e865f533995593ffac477d416a7f8b2
Roman <roman.blumovich@gmail.com>
2026-03-25 17:43:19 +0200

    fixed phone number error message bug

--- tc-marketplace/frontend/src/pages/Register.jsx
+++ tc-marketplace/frontend/src/pages/Register.jsx
@@ -100,7 +100,7 @@ const Register = () => {
                          {...register("phone")}
                         className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-green-600"
                         />
-                        {errors.confirmPassword && (
+                        {errors.phone && (
                             <p className="text-red-600 text-sm">{errors.phone.message}</p>
                         )}
                     </div>
